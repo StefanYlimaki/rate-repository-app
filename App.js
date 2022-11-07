103ae556ba03f4aa22bcc7d3943dd218b1b3ee7d
Stefan Ylimäki <stefan.ylimaki@gmail.com>
2022-11-07 15:41:42 +0200

    Task 10.3 done

--- App.js
+++ App.js
@@ -1,25 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>
-        I opened up App.js and started working on my app!
-      </Text>
-      <Text>
-        I tried Expo Go on my personal phone and on the simulated Pixel 6
-      </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import Main from './src/components/Main';
+
+const App = () => {
+  return <Main />;
+};
+
+export default App;
